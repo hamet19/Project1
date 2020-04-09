95237f66f822d62ce6d7fcdf1ac93ddbbde33a30
Hamet19 <hamettniang@gmail.com>
2020-04-09 09:34:38 +0200

    staticfiles

--- AirTrafficManagement/prod_settings.py
+++ AirTrafficManagement/prod_settings.py
@@ -6,6 +6,10 @@ TEMPLATE_DEBUG=False
 
 DATABASES['default'] = dj_database_url.config()
 
+##pour le disigne j'ai installer whitenoise
+MIDDLEWARE +=['whitenoise.middleware.WhiteNoiseMiddleware',]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+##
 SECRET_KEY='+7)7=@*14(jz#4fyf$f!29d()%=&m_t@16-yz&6xxum=lol216'
 
 ALLOWED_HOSTS=['projecthamet.herokuapp.com']
