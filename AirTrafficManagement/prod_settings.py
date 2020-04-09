bed591820baafa1cdf65104873d3840e35c7ab99
Hamet19 <hamettniang@gmail.com>
2020-04-09 02:55:06 +0200

    ajout varibale env

---
+++ AirTrafficManagement/prod_settings.py
@@ -0,0 +1,8 @@
+from AirTrafficManagement.settings import *
+
+DEBUG=False
+TEMPLATE_DEBUG=False
+
+SECRET_KEY='+7)7=@*14(jz#4fyf$f!29d()%=&m_t@16-yz&6xxum=lol216'
+
+ALLOWED_HOSTS=['projecthamet.herokuapp.com']
