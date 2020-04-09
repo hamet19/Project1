b1e45c8934a4bbe7560d3809e2585aa9fc770a00
Hamet19 <hamettniang@gmail.com>
2020-04-09 03:32:06 +0200

    install posgre

--- AirTrafficManagement/prod_settings.py
+++ AirTrafficManagement/prod_settings.py
@@ -1,7 +1,10 @@
+import dj_database_url
 from AirTrafficManagement.settings import *
 
 DEBUG=False
 TEMPLATE_DEBUG=False
+
+DATABASES['default'] = dj_database_url.config()
 
 SECRET_KEY='+7)7=@*14(jz#4fyf$f!29d()%=&m_t@16-yz&6xxum=lol216'
 
